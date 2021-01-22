a22f0779e90bfaee38c50a28b927de49d3a5903a
KHAIRAHMED <khairahmed234@gmai.com>
2021-01-22 20:47:33 +0600

    thiredTimepush

--- assignment.js
+++ assignment.js
@@ -11,32 +11,32 @@ function kilometerToMeter(kilometers) {
         return 'Dont Enter 0 or less than 0'
     }
     else {
-        return 'Dont Enter String Value!'
+        return 'Dont Enter String or array Value!'
     }
 }
 
 // console.log(kilometerToMeter());
 
-
 /*End the program of convert kilometer to meter*/
+
+
 
 
 
 /*program of Budget Calculator*/
 
 function budgetCalculator(watch, phone, laptop) {
-    if (watch, phone, laptop < 0) {
+    if ((watch < 0) || (phone < 0) || (laptop < 0)) {
         return "dont Enter less than 0"
     }
-    var total = (50 * watch + 100 * phone + laptop * 500);
-    return total;
-    
+    else {
+        var total = (50 * watch + 100 * phone + laptop * 500);
+        return total;
+    }
 
 
 }
-// console.log(budgetCalculator(5,6,7))
-
-
+// console.log(budgetCalculator())
 
 /*End the program of Budget Calculator*/
 
@@ -45,25 +45,25 @@ function budgetCalculator(watch, phone, laptop) {
 
 /*Program of Hotel Cost*/
 
-function hotelCost(day){
+function hotelCost(day) {
     var totalCost = 0;
-    if (day<=0){
+    if (day <= 0) {
         return 'Dont Enter  0 or less than 0'
     }
-    else if(day <= 10){
-        totalCost = day*100;
+    else if (day <= 10) {
+        totalCost = day * 100;
     }
-    else if(day<=20){
-        var first10DayCost = 10*100;
-        var remaining = day-10;
-        var after10DayCost = remaining*80;
-        totalCost = first10DayCost+after10DayCost
+    else if (day <= 20) {
+        var first10DayCost = 10 * 100;
+        var remaining = day - 10;
+        var after10DayCost = remaining * 80;
+        totalCost = first10DayCost + after10DayCost
     }
-    else{
-        var first10DayCost = 10*100;
-        var day10todayTo20Cost = 10*80;
-        var remaining = day-20;
-        var after20Day =remaining*50;
+    else {
+        var first10DayCost = 10 * 100;
+        var day10todayTo20Cost = 10 * 80;
+        var remaining = day - 20;
+        var after20Day = remaining * 50;
         totalCost = first10DayCost + day10todayTo20Cost + after20Day;
     }
 
@@ -72,7 +72,7 @@ function hotelCost(day){
 
 }
 
-// console.log(hotelCost(333));
+// console.log(hotelCost());
 
 /*End the Program of Hotel Cost*/
 
@@ -84,30 +84,32 @@ function hotelCost(day){
 
 function megaFriend(friendsName) {
 
-    if (friendsName.length == 0){
-        return('Please Enter A few element in array');
-    }
- 
-    else if(friendsName.length >0) {
-        var largestName = '';
-        for (var i = 0; i < friendsName.length; i++) {
-            var element = friendsName[i];
-            if (element.length > largestName.length) {
-                largestName = element;
-            }
+    if (typeof (friendsName) == ['object']) {
+
+        if (friendsName.length == 0) {
+            return ('Please enter a few element in array');
         }
-  
-    }
-    
+
+        else if (friendsName.length > 0) {
+            var largestName = '';
+            for (var i = 0; i < friendsName.length; i++) {
+                var element = friendsName[i];
+                if (element.length > largestName.length) {
+                    largestName = element;
+                }
+            }
+
+        }
         return largestName;
-    
-        
+    }
+    else {
+        return 'Invilid input. Please Enter Array'
     }
 
+}
 
 
-// console.log(megaFriend('kk'))
-
+// console.log(megaFriend([]));
 
 /*End the program of Mega Friend*/
 
